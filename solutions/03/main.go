0e45eb52a8daa476cc594935def5fd29a40ce51c
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-04 01:01:14 +1300

    Update day03 to use timing logic

--- solutions/03/main.go
+++ solutions/03/main.go
@@ -8,11 +8,13 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	debugFlag := flag.Bool("debug", false, "Debug Flag")
 	inputFilePath := flag.String("inputFile", "puzzleInput", "Path to input file.")
+	selectedPart := flag.Int("part", 0, "Part to execute. Must be 1 or 2.")
 	flag.Parse()
 	logFileHandler := SetLogging(*debugFlag)
 	defer logFileHandler.Close()
@@ -20,16 +22,34 @@ func main() {
 	inputFile, err := os.Open(*inputFilePath)
 	if err != nil {
 		slog.Error("error opening input file", "error", err)
+		os.Exit(1)
 	}
 	defer inputFile.Close()
 
 	fileScanner := bufio.NewScanner(inputFile)
-	result, err := Part02(fileScanner)
 	if err != nil {
 		slog.Error("error processing file input", "error", err)
+		os.Exit(1)
 	}
 
-	slog.Info("computation completed", "result", result)
+	var result int
+	computationStartTime := time.Now()
+	switch *selectedPart {
+	case 1:
+		result, err = Part01(fileScanner)
+	case 2:
+		result, err = Part02(fileScanner)
+	default:
+		slog.Error("invalid part selected, part must be one of 1 or 2", "part selected", *selectedPart)
+		os.Exit(1)
+	}
+	computationEndTime := time.Now()
+	if err != nil {
+		slog.Error("error encountered during computation", "error", err, "part selected", *selectedPart)
+		os.Exit(1)
+	}
+
+	slog.Info("computation completed", "result", result, "computation time elapsed (ns)", computationEndTime.Sub(computationStartTime).Nanoseconds())
 }
 
 func Part01(fileScanner *bufio.Scanner) (int, error) {
