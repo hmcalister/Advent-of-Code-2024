0f7c68ebdbd185ce2dc9e8eb835a10a330c809a0
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-07 19:47:23 +1300

    Add logic to parse a line to a calibration data struct

--- solutions/07/calibrations.go
+++ solutions/07/calibrations.go
@@ -17,3 +17,33 @@ type CalibrationData struct {
 	EquationData []int
 }
 
+func ParseLineToCalibrationData(line string) (*CalibrationData, error) {
+	lineParts := strings.Split(line, ":")
+	if len(lineParts) != 2 {
+		return nil, ErrorMalformedLine
+	}
+
+	targetNumberStr := lineParts[0]
+	targetNumber, err := strconv.Atoi(targetNumberStr)
+	if err != nil {
+		slog.Error("target number not parsable to integer", "line", line, "target number string", targetNumberStr)
+		return nil, err
+	}
+
+	equationDataStrs := strings.Split(strings.TrimSpace(lineParts[1]), " ")
+	equationData := make([]int, len(equationDataStrs))
+	for i, s := range equationDataStrs {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			slog.Error("equation data number not parsable to integer", "line", line, "equation data number string", s)
+			return nil, err
+		}
+		equationData[i] = num
+	}
+
+	return &CalibrationData{
+		TargetNumber: targetNumber,
+		EquationData: equationData,
+	}, nil
+}
+
