b25c6c8fae6bdf75d0c42780d0a887f61f044d00
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-08 19:41:41 +1300

    Add logic to parse input to antenna map

---
+++ solutions/08/antennaMap.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"log/slog"
+	"unicode"
+
+	hashset "github.com/hmcalister/Go-DSA/set/HashSet"
+)
+
+type AntennaMap struct {
+	rawMap                    [][]rune
+	width                     int
+	height                    int
+	antennaFrequencyLocations map[rune][]Coordinate
+}
+
+func ParseInputToAntennaMap(fileScanner *bufio.Scanner) *AntennaMap {
+	antennaMap := AntennaMap{
+		rawMap:                    make([][]rune, 0),
+		antennaFrequencyLocations: make(map[rune][]Coordinate),
+	}
+
+	for y := 0; fileScanner.Scan(); y += 1 {
+		line := fileScanner.Text()
+		slog.Debug("read line", "line", line)
+		antennaMap.rawMap = append(antennaMap.rawMap, []rune(line))
+
+		for x, r := range line {
+			if unicode.IsLetter(r) || unicode.IsDigit(r) {
+				c := Coordinate{x, y}
+				slog.Debug("found antenna", "coordinate", c, "frequency", r)
+				// If this is the first antenna of this frequency, make a new list for it
+				if _, ok := antennaMap.antennaFrequencyLocations[r]; !ok {
+					antennaMap.antennaFrequencyLocations[r] = make([]Coordinate, 0)
+				}
+				antennaMap.antennaFrequencyLocations[r] = append(antennaMap.antennaFrequencyLocations[r], c)
+			}
+		}
+	}
+
+	antennaMap.height = len(antennaMap.rawMap)
+	antennaMap.width = len(antennaMap.rawMap[0])
+	slog.Debug("map parsed", "antenna map", antennaMap)
+
+	return &antennaMap
+}
+
