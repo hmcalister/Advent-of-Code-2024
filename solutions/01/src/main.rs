c1a6632be984aa616a24aefbc26bb82b66cea197
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-09 02:46:31 +1300

    Add main logic for day01

---
+++ solutions/01/src/main.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use clap::Parser;
+#[allow(unused_imports)]
+use tracing::{debug, error, info, trace};
+
+mod logging;
+
+/// Program to solve Advent of Code puzzles
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct CommandLineArgs {
+    /// File to read input from
+    #[arg(long, default_value = "puzzleInput")]
+    input_file: String,
+
+    /// Selected part of puzzle to solve
+    #[arg(long, value_parser=clap::value_parser!(u8).range(1..=2))]
+    part: u8,
+}
+
+fn main() {
+    logging::set_logging();
+    let args = CommandLineArgs::parse();
+    trace!("command line args" = ?args, "parsed command line args");
+
+    let input_file_handle = File::open(args.input_file).expect("could not open input file");
+    let input_file_reader = BufReader::new(input_file_handle);
+
+    let computation_result = match args.part {
+        1 => part01(input_file_reader),
+        2 => part02(input_file_reader),
+        _ => unreachable!(), // clap has filtered out all other possibilities.
+    }
+    .expect("computation did not produce a value");
+
+    info!(
+        "computation_result" = computation_result,
+        "computation complete"
+    );
+}
+
