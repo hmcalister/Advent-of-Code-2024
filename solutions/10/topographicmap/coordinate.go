189ffd4092462f08d3f8629fcfcc21de372b9079
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-10 20:40:54 +1300

    Add coordinate struct used in topographic map

---
+++ solutions/10/topographicmap/coordinate.go
@@ -0,0 +1,15 @@
+package topographicmap
+
+type Coordinate struct {
+	X int
+	Y int
+}
+
+func (c Coordinate) GetOrthogonalNeighbors() []Coordinate {
+	return []Coordinate{
+		{c.X - 1, c.Y},
+		{c.X + 1, c.Y},
+		{c.X, c.Y - 1},
+		{c.X, c.Y + 1},
+	}
+}
