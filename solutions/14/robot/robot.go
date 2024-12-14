c32550a10b24a6a9fe88a587ee2bcdf29aa309f5
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-14 20:03:06 +1300

    Add function to compute a robots position after a certain number of steps

--- solutions/14/robot/robot.go
+++ solutions/14/robot/robot.go
@@ -12,3 +12,18 @@ func NewRobot(initialPosition, velocity Vector2) *Robot {
 	}
 }
 
+func (robot *Robot) ComputePosition(gridX, gridY int, numSteps int) Vector2 {
+	nextX := (robot.initialPosition.X + numSteps*robot.velocity.X) % gridX
+	if nextX < 0 {
+		nextX += gridX
+	}
+
+	nextY := (robot.initialPosition.Y + numSteps*robot.velocity.Y) % gridY
+	if nextY < 0 {
+		nextY += gridY
+	}
+	return Vector2{
+		X: nextX,
+		Y: nextY,
+	}
+}
