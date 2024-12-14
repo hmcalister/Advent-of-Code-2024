c4f79cf20e0a3e9385eb208110f0c3e1a6ba9581
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-14 20:02:29 +1300

    Add function to parse input file to robot struct

--- solutions/14/main.go
+++ solutions/14/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
+	"fmt"
+	"hmcalister/AdventOfCode/robot"
 	"log/slog"
 	"os"
+	"regexp"
 	"runtime/pprof"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -63,6 +69,45 @@ func main() {
 	}
 
 	slog.Info("computation completed", "result", result, "computation time elapsed (ns)", computationEndTime.Sub(computationStartTime).Nanoseconds())
+}
+
+func parseInputToRobots(fileScanner *bufio.Scanner) []*robot.Robot {
+	robotRegex := regexp.MustCompile(`p=([+-]?\d*),([+-]?\d*) v=([+-]?\d*),([+-]?\d*)`)
+	robots := make([]*robot.Robot, 0)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		matches := robotRegex.FindStringSubmatch(line)
+		if matches == nil || len(matches) != 5 {
+			slog.Error("line does not match expected robot data format", "line", line, "matches", matches)
+			continue
+		}
+
+		px, pxErr := strconv.Atoi(matches[1])
+		py, pyErr := strconv.Atoi(matches[2])
+		vx, vxErr := strconv.Atoi(matches[3])
+		vy, vyErr := strconv.Atoi(matches[4])
+		if errors.Join(pxErr, pyErr, vxErr, vyErr) != nil {
+			slog.Error("could not parse matched groups to robot data",
+				"line", line,
+				"matches", matches,
+				"position x error", pxErr,
+				"position y error", pyErr,
+				"velocity x error", vxErr,
+				"velocity y error", vyErr,
+			)
+			continue
+		}
+
+		newRobot := robot.NewRobot(
+			robot.Vector2{X: px, Y: py},
+			robot.Vector2{X: vx, Y: vy},
+		)
+		slog.Debug("parsed robot", "line", line, "robot", *newRobot)
+		robots = append(robots, newRobot)
+	}
+
+	return robots
 }
 
 func Part01(fileScanner *bufio.Scanner) (int, error) {
