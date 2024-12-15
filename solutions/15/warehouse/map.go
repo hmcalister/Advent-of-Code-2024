6b2c020a992992007b1df3a57358853cbdf08dc6
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-15 21:20:58 +1300

    Add parsing for warehouse map

--- solutions/15/warehouse/map.go
+++ solutions/15/warehouse/map.go
@@ -22,3 +22,32 @@ type WarehouseMap struct {
 	mapHeight     int
 }
 
+func NewWarehouseMap(warehouseMapStrs []string) *WarehouseMap {
+	warehouse := &WarehouseMap{
+		wallMap:   hashset.New[gridutils.Coordinate](),
+		boxMap:    hashset.New[gridutils.Coordinate](),
+		mapWidth:  len(warehouseMapStrs[0]),
+		mapHeight: len(warehouseMapStrs),
+	}
+
+	for y, row := range warehouseMapStrs {
+		slog.Debug("parsing row", "row", row)
+		for x, cell := range row {
+			currentCoordinate := gridutils.Coordinate{X: x, Y: y}
+			switch cell {
+			case WALL_RUNE:
+				slog.Debug("found wall", "coordinate", currentCoordinate)
+				warehouse.wallMap.Add(currentCoordinate)
+			case BOX_RUNE:
+				slog.Debug("found box", "coordinate", currentCoordinate)
+				warehouse.boxMap.Add(currentCoordinate)
+			case ROBOT_RUNE:
+				slog.Debug("found robot", "coordinate", currentCoordinate)
+				warehouse.robotPosition = currentCoordinate
+			}
+		}
+	}
+
+	return warehouse
+}
+
