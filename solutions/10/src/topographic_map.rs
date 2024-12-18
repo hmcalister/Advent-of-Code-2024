f05b7ee709d79098a1c22ce263a70236ff2674ba
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-18 20:23:46 +1300

    Add recursive function for part 02

--- solutions/10/src/topographic_map.rs
+++ solutions/10/src/topographic_map.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[allow(unused_imports)]
 use tracing::{debug, error, info, trace};
@@ -51,9 +51,43 @@ pub fn new_topographic_map(map_data: Vec<String>) -> TopographicMap {
 }
 
 impl TopographicMap {
-    /// Find and compute the score (the number of reachable max height coordinates from this one) of a given coordinate
+    /// Find and return the unique max height coordinates reachable from a given coordinate.
     /// Note the given coordinate may not have a height of zero to allow for a recursive solution.
-    fn find_coordinate_score(&self, coordinate: LatticeVector2D) -> i64 {
+    fn find_reachable_trailends(&self, coordinate: LatticeVector2D) -> HashSet<LatticeVector2D> {
+        let mut reachable_trailends = HashSet::new();
+
+        // If we do not have a height, then we cannot have a score
+        let Some(&current_height) = self.coordinate_heights.get(&coordinate) else {
+            return reachable_trailends;
+        };
+
+        // If we are already max height we are done
+        if current_height == self.max_height {
+            reachable_trailends.insert(coordinate);
+            return reachable_trailends;
+        };
+
+        // We are not max height, so walk over neighbors and sum their heights
+        let _: Vec<_> = coordinate
+            .iter_neighbors()
+            .filter(|neighbor| {
+                let Some(&neighbor_height) = self.coordinate_heights.get(&neighbor) else {
+                    return false;
+                };
+                neighbor_height == current_height + 1
+            })
+            .map(|neighbor| {
+                let neighbor_reachable_trailheads = self.find_reachable_trailends(neighbor);
+                reachable_trailends.extend(neighbor_reachable_trailheads);
+            })
+            .collect();
+
+        reachable_trailends
+    }
+
+    /// Find and return the number of paths to max height coordinates from a given coordinate.
+    /// Note the given coordinate may not have a height of zero to allow for a recursive solution.
+    fn find_coordinate_rating(&self, coordinate: LatticeVector2D) -> i64 {
         // If we do not have a height, then we cannot have a score
         let Some(&current_height) = self.coordinate_heights.get(&coordinate) else {
             return 0;
@@ -73,15 +107,30 @@ impl TopographicMap {
                 };
                 neighbor_height == current_height+1
             })
-            .map(|neighbor| self.find_coordinate_score(neighbor))
+            .map(|neighbor| self.find_coordinate_rating(neighbor))
             .sum()
     }
 
     pub fn count_trailhead_scores(&self) -> i64 {
-        self.trailheads.iter().map(|trailhead| {
-            let trailhead_score = self.find_coordinate_score(*trailhead);
-            debug!(?trailhead, ?trailhead_score, "found score for trailhead");
-            trailhead_score
-        }).sum()
+        self.trailheads
+            .iter()
+            .map(|trailhead| {
+                let reachable_trailheads = self.find_reachable_trailends(*trailhead);
+                let trailhead_score = reachable_trailheads.len() as i64;
+                debug!(?trailhead, ?trailhead_score, "found score for trailhead");
+                trailhead_score
+            })
+            .sum()
+    }
+
+    pub fn count_trailhead_ratings(&self) -> i64 {
+        self.trailheads
+            .iter()
+            .map(|trailhead| {
+                let trailhead_rating = self.find_coordinate_rating(*trailhead);
+                debug!(?trailhead, ?trailhead_rating, "found rating for trailhead");
+                trailhead_rating
+            })
+            .sum()
     }
 }
