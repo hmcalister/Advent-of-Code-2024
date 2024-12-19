638736029b369ad5ee8a259ebbed6ff8d4ebb87a
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-19 18:56:18 +1300

    Add parsing logic to main

---
+++ solutions/19/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"hmcalister/AdventOfCode/towel"
+	"log/slog"
+	"os"
+	"runtime/pprof"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	CPU_PROFILE_FILEPATH string = "profile"
+)
+
+func main() {
+	debugFlag := flag.Bool("debug", false, "Debug Flag")
+	inputFilePath := flag.String("inputFile", "puzzleInput", "Path to input file.")
+	selectedPart := flag.Int("part", 0, "Part to execute. Must be 1 or 2.")
+	profile := flag.Bool("profile", false, "Flag to profile program")
+	flag.Parse()
+	if *profile {
+		f, err := os.Create(CPU_PROFILE_FILEPATH)
+		if err != nil {
+			slog.Error("could not create cpu profile file", "file", CPU_PROFILE_FILEPATH)
+			os.Exit(1)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	logFileHandler := SetLogging(*debugFlag)
+	defer logFileHandler.Close()
+
+	inputFile, err := os.Open(*inputFilePath)
+	if err != nil {
+		slog.Error("error opening input file", "error", err)
+		os.Exit(1)
+	}
+	defer inputFile.Close()
+
+	fileScanner := bufio.NewScanner(inputFile)
+	if err != nil {
+		slog.Error("error processing file input", "error", err)
+		os.Exit(1)
+	}
+
+	var result int
+	computationStartTime := time.Now()
+	switch *selectedPart {
+	case 1:
+		result, err = Part01(fileScanner)
+	case 2:
+		result, err = Part02(fileScanner)
+	default:
+		slog.Error("invalid part selected, part must be one of 1 or 2", "part selected", *selectedPart)
+		os.Exit(1)
+	}
+	computationEndTime := time.Now()
+	if err != nil {
+		slog.Error("error encountered during computation", "error", err, "part selected", *selectedPart)
+		os.Exit(1)
+	}
+
+	slog.Info("computation completed", "result", result, "computation time elapsed (ns)", computationEndTime.Sub(computationStartTime).Nanoseconds())
+}
+
+func parseInput(fileScanner *bufio.Scanner) ([]string, []string) {
+	if !fileScanner.Scan() {
+		slog.Error("could not read towel atom line")
+		os.Exit(1)
+	}
+
+	towelAtomsStr := fileScanner.Text()
+	towelAtoms := strings.Split(towelAtomsStr, ", ")
+
+	fileScanner.Scan()
+
+	targetPatterns := make([]string, 0)
+	for fileScanner.Scan() {
+		targetPatterns = append(targetPatterns, fileScanner.Text())
+	}
+
+	return towelAtoms, targetPatterns
+}
+
