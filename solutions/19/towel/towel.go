7474bf66c30f66bcf1db30828943a83093a906d4
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-19 18:55:53 +1300

    Add logic for a recursive approach to part01

--- solutions/19/towel/towel.go
+++ solutions/19/towel/towel.go
@@ -15,3 +15,31 @@ func NewTowelCollection(towelAtoms []string) TowelCollection {
 	}
 }
 
+// Given the (remaining) pattern to create, returns an array of atoms used to create the pattern
+// or nil if the pattern is impossible
+func (towel TowelCollection) isPatternValidRecursive(remainingPattern string) []string {
+	if len(remainingPattern) == 0 {
+		return make([]string, 0)
+	}
+
+	for _, atom := range towel.towelAtoms {
+		if patternLessAtom, hasPrefix := strings.CutPrefix(remainingPattern, atom); hasPrefix {
+			// slog.Debug("found prefix", "remaining pattern", remainingPattern, "prefix atom", atom, "pattern less atom", patternLessAtom)
+			if constructingAtoms := towel.isPatternValidRecursive(patternLessAtom); constructingAtoms != nil {
+				constructingAtoms = append(constructingAtoms, atom)
+				return constructingAtoms
+			}
+		}
+	}
+	return nil
+}
+func (towel TowelCollection) IsPatternValid(pattern string) bool {
+	constructingAtoms := towel.isPatternValidRecursive(pattern)
+	if constructingAtoms != nil {
+		slog.Debug("constructed pattern successfully", "pattern", pattern, "atoms", constructingAtoms)
+		return true
+	} else {
+		return false
+	}
+}
+
